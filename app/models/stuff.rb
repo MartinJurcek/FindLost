e7b4ef6809345b74052842764a7080727b8d4990
MartinJurcek <jurcekmartin@gmail.com>
2020-12-07 21:20:53 +0100

    added option for upload images

--- app/models/stuff.rb
+++ app/models/stuff.rb
@@ -1,8 +1,43 @@
 class Stuff < ApplicationRecord
+  has_one_attached :image
   belongs_to :user
   has_many :stuff_categories
   has_many :categories, through: :stuff_categories
   validates :title, presence: true, length: { maximum: 20 }
-  validates :description, presence: true, length: {maximum: 200}
+  validates :description, presence: true, length: {maximum: 300}
+  #validate :image_type
+  after_commit :add_default_image, on: %i[create update]
 
+  def stuff_image
+    if image.attached?
+      image.variant(resize: "150x150!").processed 
+    else
+      "/image-not-found.png"
+    end
+  end
+
+  private
+
+  #def image_type
+  #  if image.attached? == false
+  #    errors.add(:image, "are missing")
+  #  elsif 
+  #    !image.content_type.in?(%('image/jpeg image/png')) 
+  #    errors.add(:image, 'needs to be a JPEG or PNG')
+  #  end
+  #end
+
+  def add_default_image
+    unless image.attached?
+      image.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'image-not-found.png'
+          )
+        ),
+        filename: 'image-not-found.png',
+        content_type: 'image/jpg'
+      )
+    end
+  end
 end
