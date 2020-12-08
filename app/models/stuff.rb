0de91974d563685ca14d9cf3de5c86eba837ef37
MartinJurcek <jurcekmartin@gmail.com>
2020-12-08 22:12:12 +0100

    upload images views

--- app/models/stuff.rb
+++ app/models/stuff.rb
@@ -8,6 +8,7 @@ class Stuff < ApplicationRecord
   #validate :image_type
   after_commit :add_default_image, on: %i[create update]
 
+  
   def stuff_image
     if image.attached?
       image.variant(resize: "150x150!").processed 
@@ -18,14 +19,14 @@ class Stuff < ApplicationRecord
 
   private
 
-  #def image_type
-  #  if image.attached? == false
-  #    errors.add(:image, "are missing")
-  #  elsif 
-  #    !image.content_type.in?(%('image/jpeg image/png')) 
-  #    errors.add(:image, 'needs to be a JPEG or PNG')
-  #  end
-  #end
+  def image_type
+    if image.attached? == false
+      add_default_image
+    elsif
+      !image.content_type.in?(%('image/jpeg image/png')) 
+      errors.add(:image, 'needs to be a JPEG or PNG')
+    end
+  end
 
   def add_default_image
     unless image.attached?
@@ -36,7 +37,7 @@ class Stuff < ApplicationRecord
           )
         ),
         filename: 'image-not-found.png',
-        content_type: 'image/jpg'
+        content_type: 'image/png'
       )
     end
   end
